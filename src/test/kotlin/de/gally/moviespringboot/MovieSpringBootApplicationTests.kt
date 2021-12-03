e823e8569cf31952c9e5b772d6cd86026953dd39
Tioman Gally <tioman.gally@novatec-gmbh.de>
2021-12-03 09:18:39 +0100

    Movie: First commit

---
+++ src/test/kotlin/de/gally/moviespringboot/MovieSpringBootApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.gally.moviespringboot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MovieSpringBootApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
